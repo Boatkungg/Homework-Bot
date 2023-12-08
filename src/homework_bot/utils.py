d1fe97c01c286b6be7d8d88171b3ed492f56b775
B_K <chayanongg2550@gmail.com>
2023-12-08 18:36:28 +0700

    WIP

--- src/homework_bot/utils.py
+++ src/homework_bot/utils.py
@@ -1,3 +1,4 @@
+import calendar
 from datetime import datetime
 from typing import List, Union
 
@@ -26,3 +27,23 @@ def pretty_time(seconds):
         return f"{str(round(seconds * 1000)).zfill(3)}ms"
 
     return f"{str(round(seconds * 1000000)).zfill(3)}µs"
+
+
+def calendar_label(year, month):
+    flat_calendar = sum(calendar.monthcalendar(year, month), [])
+    flat_calendar[0:0] = [0]  # insert 0 at the beginning of the list
+    flat_calendar.extend([0] * (7 - (len(flat_calendar) % 7)))  # pad the end of the list with 0s
+    flat_calendar = [
+        str(i) if i != 0 else "" for i in flat_calendar
+    ]  # convert to strings and replace 0s with empty strings
+
+    reshaped_calendar = [
+        flat_calendar[(i * 7) : (7 * (i + 1))] for i in range(len(flat_calendar) // 7)
+    ]
+    if str().join(reshaped_calendar[0]) == "":
+        del reshaped_calendar[0]
+
+    if str().join(reshaped_calendar[-1]) == "":
+        del reshaped_calendar[-1]
+
+    return reshaped_calendar
